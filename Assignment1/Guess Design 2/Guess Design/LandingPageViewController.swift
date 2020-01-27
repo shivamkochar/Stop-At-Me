d2c40f4524fa8fc5d2190407d4fc2e8136f47db1
Shivam Kochar <shivam.kochar7@gmail.com>
2020-01-26 21:44:40 -0500

    Initial

---
+++ Assignment1/Guess Design 2/Guess Design/LandingPageViewController.swift
@@ -0,0 +1,32 @@
+//
+//  LandingPageViewController.swift
+//  Guess Design
+//
+//  Created by Student on 2019-10-13.
+//  Copyright © 2019 Student. All rights reserved.
+//
+
+import UIKit
+
+class LandingPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+
+    }
+    
+    @IBOutlet weak var titleLbl: UILabel!
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
